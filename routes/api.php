3938e7267e165dcab38f1cd1b58039d6eb8af5e4
Theodor Georgopoulos <thodorisgr0@hotmail.com>
2018-01-03 19:08:29 +0200

    api routes updated

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,4 @@ $route->get('home','HomeController@index')->name('API_Home');
 
 $route->get('/', 'HomeController@index');
 
-$route->get('about','AboutController@index');
 
-$route->post('form', 'HomeController@index')->name('apiPost');
