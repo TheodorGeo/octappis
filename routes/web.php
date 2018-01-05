b84e4cfb1d8ed1dcce0c323ae1921437d42d3394
Theodor Georgopoulos <thodorisgr0@hotmail.com>
2018-01-05 19:19:08 +0200

    mino changes : remove route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 
 $route->get('/','ProfileController@show');
 
-/*$route->get('home','HomeController@index')->directory('asds')->name('HomePage');*/
-
 $route->get('about','AboutController@index')->name('AboutPage');
 
 $route->post('about','AboutController@post')->name('PostPage');
